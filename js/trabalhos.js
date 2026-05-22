71ac5708e99852ffa506215d0d954debcb8029f7
Paulo Brito <psnbrito@gmail.com>
2026-05-22 12:26:58 -0100

    Add files via upload

---
+++ js/trabalhos.js
@@ -0,0 +1,35 @@
+// js/trabalhos.js
+const trabalhos = {
+  portofolio: {
+    titulo: "Convites - Cartazes - Flyers - Menus Restaurantes - Panfletos - Logotipos",
+    categoria: "Design Gráfico",
+    cliente: "Diversos",
+    descricao: "Aqui pode encontrar um conjunto de trabalhos desenvolvido e elaborados por mim. Ultimamente trabalho com Beauty Plaza SPA e Suítes, na elaboração de diversos Flyers alusivos aos serviços prestados pela empresa. Elaborei o Logótipo da Associação Regional Hípica do Sal e também flyers e cartazes de atividades realizados, também elaborei o logótipo de Vale de Baboso/Dias'Horses. Tenho feito trabalhos gráficos para a Escolinha Menifute, equipa da Nova Geração, Desport Salgadinh, Farmacia D. Ivete, Store Internacional, Lda., Restaurante Silva's, GDRC Pretoria e além de diversos trabalhos como convites e outros para particulares e pessoas individuais.",
+    ferramentas: ["Canva", "Affinity Publisher", "GIMP"],
+    imagens: [
+      "image/portfolio/Trabalho (0).png",
+      "image/portfolio/Trabalho (1).png",
+      "image/portfolio/Trabalho (2).png",
+      "image/portfolio/Trabalho (3).png",
+      "image/portfolio/Trabalho (4).png",
+	    "image/portfolio/Trabalho (5).png",
+	    "image/portfolio/Trabalho (6).png",	  
+	    "image/portfolio/Trabalho (7).png",	
+	    "image/portfolio/Trabalho (8).png",	
+    	"image/portfolio/Trabalho (9).png",	
+	    "image/portfolio/Trabalho (10).png",	
+	    "image/portfolio/Trabalho (11).png",	
+	    "image/portfolio/Trabalho (12).png",	
+	    "image/portfolio/Trabalho (13).png",	
+	    "image/portfolio/Trabalho (14).png",	
+	    "image/portfolio/Trabalho (15).png",	
+	    "image/portfolio/Trabalho (16).png",	
+	    "image/portfolio/Trabalho (17).png",	
+	    "image/portfolio/Trabalho (18).png",	
+	    "image/portfolio/Trabalho (19).png",	
+	    "image/portfolio/Trabalho (20).png",	
+	    "image/portfolio/Trabalho (21).png",	
+      "image/portfolio/Trabalho (22).png"
+    ]
+  },
+};
